3da177dde330a809a4117111b89196c529725e58
ki-2_iva <www.ivanovda@gmail.com>
2025-06-02 21:20:54 +0300

    task 7.3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   products:
     "https://g2ewm6wfnf.execute-api.us-east-1.amazonaws.com/prod/products",
   order: "https://g2ewm6wfnf.execute-api.us-east-1.amazonaws.com/prod",
-  import: "https://g2ewm6wfnf.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://6mnsfk2ynl.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://g2ewm6wfnf.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://g2ewm6wfnf.execute-api.us-east-1.amazonaws.com/prod",
 };
